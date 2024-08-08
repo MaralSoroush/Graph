66c8ec70d2368d0f8df7132171714048cc30fbf7
Maral <maralsoroush23@gmail.com>
2024-08-08 21:19:33 +0330

    0006: fix cache

--- graphproject/tools/middlewares.py
+++ graphproject/tools/middlewares.py
@@ -13,14 +13,14 @@ class EndPointCallMiddleware:
         cache_key = f"api_call_count:{path}"
         call_count = cache.get(cache_key, 0)
         call_count += 1
-        cache.set(cache_key, call_count)
+        cache.set(cache_key, call_count, timeout=None)
         endpoint, created = EndPointCall.objects.get_or_create(path=path)
 
         if call_count and call_count % 100 == 0:
             endpoint.call_count += call_count
             endpoint.save()
             # Reset the counter in Redis
-            cache.set(cache_key, 0)
+            cache.set(cache_key, 0, timeout=None)
 
         response = self.get_response(request)
 
